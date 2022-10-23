a1ff454e87c5ed094698268366077e068d7d2c89
Иван Шихалев <shikhalev@gmail.com>
2022-10-23 23:20:37 +0500

    move initialization to global level

--- lua/hwmon.lua
+++ lua/hwmon.lua
@@ -1,35 +1,28 @@
 require 'utils'
 
-local hwmon_detected = false
 local hwmon_indices = {}
-local function hwmon_detect()
-    if hwmon_detected then
-        return
+local idx = 0
+while true do
+    local path = '/sys/class/hwmon/hwmon' .. idx
+    local file = io.open(path .. '/name', 'r')
+    if file == nil then 
+        break
     end
-    local idx = 0
-    while true do
-        local path = '/sys/class/hwmon/hwmon' .. idx
-        local file = io.open(path .. '/name', 'r')
-        if file == nil then 
-            break
-        end
-        local name = file:read("*a"):gsub('\n', '')
-        file:close()
-        if name == 'nvme' then
-             name = readlink(path):match('nvme%d+')
-        end
-        hwmon_indices[name] = idx
-        idx = idx + 1
+    local name = file:read("*a"):gsub('\n', '')
+    file:close()
+    if name == 'nvme' then
+         name = readlink(path):match('nvme%d+')
     end
-    hwmon_detected = true
+    hwmon_indices[name] = idx
+    idx = idx + 1
 end
 
 function conky_hwmon_index(device)
-    hwmon_detect()
+--    hwmon_detect()
     return hwmon_indices[device]
 end
 
 function conky_hwmon(device, type, index)
-    hwmon_detect()
+--    hwmon_detect()
     return conky_parse('${hwmon ' .. hwmon_indices[device] .. ' ' .. type .. ' ' .. index .. '}')
 end
