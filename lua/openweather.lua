086b72f8989d916e71a002731f49f60d9adb4b4d
Иван Шихалев <shikhalev@gmail.com>
2022-11-03 16:56:24 +0500

    openweather draft done

--- lua/openweather.lua
+++ lua/openweather.lua
@@ -1,7 +1,26 @@
-package.path = package.path .. ';' .. os.getenv('HOME') .. '/.config/conky/?.lua'
 local cjson = require 'cjson'
-weather = {}
+-- require 'utils'
+weather = {
+    places = {}
+}
+package.path = package.path .. ';' .. os.getenv('HOME') .. '/.config/conky/?.lua'
 require 'openweather_config'
+
+local weather_url = 'http://api.openweathermap.org/data/2.5/forecast?'
+local icons_url = 'http://openweathermap.org/img/w/%s.png'
+local cache_path = os.getenv('HOME') .. '/.cache/openweather/'
+local data_path = os.getenv('HOME') .. '/.local/conky/openweather/'
+
+local default_update_interval = 60 * 60
+
+if weather.config.icon_cache_path == nil then
+    weather.config.icon_cache_path = cache_path .. 'icons/'
+end
+os.execute('mkdir -p ' .. weather.config.icon_cache_path)
+if weather.config.data_path == nil then
+    weather.config.data_path = data_path
+end
+os.execute('mkdir -p ' .. weather.config.data_path)
 
 function conky_cjson_test()
     -- return 'XXX'
@@ -9,88 +28,152 @@ function conky_cjson_test()
 end
 
 local function load_place(place)
-    -- TODO: implement
+    local config = weather.config.places[place]
+    if config == nil then
+        error('Config ' .. place .. ' not found!')
+    end
+    
+    -- check timing
+    local update_interval = config.update_interval or weather.config.update_interval or default_update_interval
+    local current_time = os.time()
+    if config.updated and config.updated > current_time - update_interval then
+        return weather.places[place]
+    end
+
+    local url = weather_url .. 'appid=' .. weather.config.api_key
+    if weather.config.units then
+        url = url .. '&units=' .. weather.config.units
+    end
+    if weather.config.lang then
+        url = url .. '&lang=' .. weather.config.lang
+    end
+    if config.latitude then
+        url = url .. '&lat=' .. config.latitude
+    end
+    if config.longitude then
+        url = url .. '&lon=' .. config.longitude
+    end
+    if config.city_id then
+        url = url .. '&id=' .. config.city_id
+    end
+    local datafile = weather.config.data_path .. place .. '-' .. current_time .. '.json'
+    if download_file(url, datafile) then
+        weather.places[place] = cjson.decode(readfile(datafile))
+        weather.config.places[place].updated = current_time
+    end
+    return weather.places[place]
+end
+
+function conky_openweather_url(place)
+    local config = weather.config.places[place]
+    if config == nil then
+        error('Config ' .. place .. ' not found!')
+    end
+    
+    local url = weather_url .. 'appid=' .. weather.config.api_key
+    if weather.config.units then
+        url = url .. '&units=' .. weather.config.units
+    end
+    if weather.config.lang then
+        url = url .. '&lang=' .. weather.config.lang
+    end
+    if config.latitude then
+        url = url .. '&lat=' .. config.latitude
+    end
+    if config.longitude then
+        url = url .. '&lon=' .. config.longitude
+    end
+    if config.city_id then
+        url = url .. '&id=' .. config.city_id
+    end
+    return url
 end
 
 function conky_openweather_updated(place)
-    load_place(place)
-    return weather.places[place].updated
+    return weather.config.places[place].updated
 end
 
 function conky_openweather_city_id(place)
-    load_place(place)
-    return weather.places[place].city.id
+    local data = load_place(place)
+    return data.city.id
 end
 
 function conky_openweather_city_name(place)
-    load_place(place)
-    return weather.places[place].city.name
+    local data = load_place(place)
+    return data.city.name
 end
 
 function conky_openweather_sunset(place, format)
-    load_place(place)
-    local sunset = tonumber(weather.places[place].city.sunset) + tonumber(weather.places[place].city.timezone)
+    local data = load_place(place)
+    local sunset = tonumber(data.city.sunset) + tonumber(data.city.timezone)
     return os.date(format, sunset)
 end
 
 function conky_openweather_sunrise(place, format)
-    load_place(place)
-    local sunrise = tonumber(weather.places[place].city.sunrise) + tonumber(weather.places[place].city.timezone)
+    local data load_place(place)
+    local sunrise = tonumber(data.city.sunrise) + tonumber(data.city.timezone)
     return os.date(format, sunrise)
 end
 
 function conky_openweather_count(place)
-    load_place(place)
-    return weather.places[place].cnt
+    local data = load_place(place)
+    return data.cnt
 end
 
 function conky_openweather_weather_time(place, index, format)
-    load_place(place)
-    local time = tonumber(weather.places[place].list[index].dt) + tonumber(weather.places[place].city.timezone)
+    local data = load_place(place)
+    local time = tonumber(data.list[tonumber(index)].dt) + tonumber(data.city.timezone)
     return os.date(format, time)
 end
 
 function conky_openweather_weather_temp(place, index)
-    load_place(place)
-    return weather.places[place].list[index].main.temp
+    local data = load_place(place)
+    return data.list[tonumber(index)].main.temp
 end
 
 function conky_openweather_weather_temp_min(place, index)
-    load_place(place)
-    return weather.places[place].list[index].main.temp_min
+    local data = load_place(place)
+    return data.list[tonumber(index)].main.temp_min
 end
 
 function conky_openweather_weather_temp_max(place, index)
-    load_place(place)
-    return weather.places[place].list[index].main.temp_max
+    local data = load_place(place)
+    return data.list[tonumber(index)].main.temp_max
 end
 
 function conky_openweather_weather_pressure(place, index)
-    load_place(place)
-    return weather.places[place].list[index].main.pressure
+    local data = load_place(place)
+    return data.list[tonumber(index)].main.pressure
 end
 
 function conky_openweather_weather_humidity(place, index)
-    load_place(place)
-    return weather.places[place].list[index].main.humidity
+    local data = load_place(place)
+    return data.list[tonumber(index)].main.humidity
 end
 
 function conky_openweather_weather_id(place, index)
-    load_place(place)
-    return weather.places[place].list[index].weather.id
+    local data = load_place(place)
+    return data.list[tonumber(index)].weather.id
 end
 
 function conky_openweather_weather_description(place, index)
-    load_place(place)
-    return weather.places[place].list[index].weather.description
+    local data = load_place(place)
+    return data.list[tonumber(index)].weather.description
 end
 
 function conky_openweather_weather_icon(place, index)
-    load_place(place)
-    return weather.places[place].list[index].weather.icon
+    local data = load_place(place)
+    return data.list[tonumber(index)].weather.icon
 end
 
-function conky_openweather_weather_icon_file(place, index)
-    load_place(place)
-    -- TODO: load with cache
+function conky_openweather_weather_icon_filename(place, index)
+    local icon = conky_openweather_weather_icon(place, index)
+    local filename = weather.options.icon_cache_path .. icon .. ".png"
+    if not fileexists(filename) then
+        local url = string.format(icons_url, icon)
+        if not download_file(url, filename) then
+            return nil
+        end
+    end
+    return filename
 end
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,3 +1,5 @@
+local posix_stat = require 'posix.sys.stat'
+local http = require 'socket.http'
 
 function round(value)
     return math.floor(value + 0.5)
@@ -16,4 +18,30 @@ function readlink(path)
     local result = handle:read('*a'):gsub('\n', '')
     handle:close()
     return result
+end
+
+function readfile(path)
+    local handle = io.open(path, 'r')
+    local result = handle:read('*a')
+    handle:close()
+    return result
+end
+
+function fileexists(path)
+    local stat = posix_stat.stat(path)
+    return not (stat == nil or type(stat) == 'string' or type(stat) == 'number')
+end
+
+function download_file(url, filename)
+    local body, code = http.request(url)
+    if not body then
+        return nil
+    end
+    local handle = io.open(filename, 'wb')
+    if not handle then
+        return nil
+    end
+    handle:write(body)
+    handle:close()
+    return filename
 end
